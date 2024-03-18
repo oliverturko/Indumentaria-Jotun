a672d1a72281c1221b577233dbcd0545833deed7
oliverturko <oliverturko@hotmail.com>
2024-03-17 22:57:21 -0300

    Segunda pre entrega, le agregue arrays

--- java/main.js
+++ java/main.js
@@ -1,49 +1,105 @@
 alert("Bienvenido a Indumentaria Jotun");
 let nombre = prompt("Ingrese su nombre");
-const Remera=10;
-const Pantalon=15;
-const Short=5;
-
-function clickbtn(){
-const operacionValida = true;
-let prendaSeleccionada;
-do {
-  let compra = parseFloat(prompt("Hola "+ nombre + "!!" + "\nSeleccione que clase de prenda quiere comprar: \n1.Remera \n2.Pantalon \n3.Short"));
-  switch(compra) {
-    case 1:
-      prendaSeleccionada = Remera;
-      break;
-    case 2:
-      prendaSeleccionada = Pantalon;
-      break;
-    case 3:
-      prendaSeleccionada= Short;
-      break;
-    default:
-      operacionValida = false;
-      break;
-}
-  if(operacionValida) {
-   let cantidad = parseFloat(prompt("Cuantas unidades?"))
-   const resultado = validarYCalcular(cantidad,prendaSeleccionada)
-   if (typeof resultado === 'string')
-       alert(resultado);
-    else {
-      alert("Su compra tiene un monto de $ " + resultado)
-    }
-  } else {
-   alert("Operacion invalida");
+const productos = [
+  {
+      nombre: "Remera",
+      precio: 1500,
+      id:1,
+  },
+  {
+      nombre: "Pantalon",
+      precio: 3000,
+      id:2,
+  },
+  {
+      nombre: "Short",
+      precio: 2000,
+      id:3,
   }
-} while(!operacionValida);
+]
+function seleccionProducto() {
+
+  // Ingrese el producto
+  const codigoProductoSeleccionado = parseFloat(prompt("Hola "+ nombre + "!!" + "\nSeleccione que clase de prenda quiere comprar: \n1.Remera \n2.Pantalon \n3.Short"));
+  const productoSeleccionado = productos.find(producto => {
+    return producto.id === codigoProductoSeleccionado;
+  })
+  if (!productoSeleccionado) return null;
+  return productoSeleccionado;
 }
 
-function validarYCalcular(cant, obj){
+function pedirCantidad (precio) {
+  const cantidad = parseFloat(prompt("Cuantas unidades?")) // Ingrese la cantidad
+  // Validar y calcular la cantidad
+  const resultado = validarYCalcular(cantidad, precio);
+  return {cantidad,preciototal:resultado};
+}
+
+function validarYCalcular(cantidad, precio){
   let res = 0;
-    if(!isNaN(cant)){
-        res=obj*cant;
-        return res;
+    if(!isNaN(cantidad)){
+      res=precio*cantidad;
+      return res;
     }
     else{
     return "Ingrese un numero";
   }
 }
+
+function mostrarTotalDeCompra (total) {
+   alert('El total de la compra $' + total);
+}
+
+function seleccionarYMostrarListadoProductos () {
+  let deseaContinuarCompra = 'NO';
+  const productosEnLaCompra = [];
+  do {
+    // Pido el producto
+    const producto = seleccionProducto();
+    // Valido si el producto existe
+    if (producto) {
+      const productoAComprar = {
+        nombre: producto.nombre,
+        precio: producto.precio,
+        cantidad: 0,
+        id: producto.id
+      }
+      // Pido cantidad
+      const resultado = pedirCantidad(productoAComprar.precio);
+      // Valido la cantidad
+      if (typeof resultado.preciototal === 'string') {
+        alert('cantidad invalida');
+      }
+      else {
+        productoAComprar.cantidad = resultado.cantidad;
+        productoAComprar.precio = resultado.preciototal;
+        // El producto se ingreso, calculo y valido correctamente entonces lo agrego al carro
+        productosEnLaCompra.push(productoAComprar);
+      }
+    } else {
+      alert('productoInvalido');
+    }
+
+    deseaContinuarCompra = prompt("Desea continuar comprando? (SI/NO)")
+    console.log(deseaContinuarCompra)
+  } while (deseaContinuarCompra.toUpperCase() === 'SI');
+  return productosEnLaCompra;
+}
+
+function clickbtn () {
+  const carro = seleccionarYMostrarListadoProductos();
+  if (carro.length === 0) {
+    alert('El carro esta vacio')
+    return;
+  }
+  let totalCarro = 0;
+  let resumenDeCompra = '';
+  carro.forEach((elementoDelCarro) => {
+    totalCarro += elementoDelCarro.precio;
+    resumenDeCompra += '\n\n Nombre del producto ' + elementoDelCarro.nombre;
+    resumenDeCompra += '\n Cantidad ' + elementoDelCarro.cantidad;
+    resumenDeCompra += '\n Precio ' + elementoDelCarro.precio;
+
+    })
+    alert(resumenDeCompra)
+   mostrarTotalDeCompra(totalCarro);}
